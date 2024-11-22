5c371ee293d1fdfd1820570a8e4ad3f6793369ab
javidominguezsegura <javier.dominguez.segura@gmail.com>
2024-11-22 16:30:15 +0100

    more documentation

--- src/others/algorithms/searching.py
+++ src/others/algorithms/searching.py
@@ -3,30 +3,26 @@
 class MySearching():
     def binary_search_leaderboard(self, leaderboard_data: list, userRank: str) -> tuple[str, int, int]:
         """ 
-            Analysis:
-            - Time complexity: O(log n)
+            Analysis (for binary search):
+            - Time complexity: O(log n) / worst case; O(1) / best case
             - Space complexity: O(1)
         """
 
         left, right = 0, len(leaderboard_data) - 1
-
-        print(f"Leaderboard data: {leaderboard_data}")
         
         while left <= right:
             mid = (left + right) // 2
             rank = leaderboard_data[mid][0]  
-
-            print(f"Rank: {rank}, userRank: {userRank}, current user: {leaderboard_data[mid][1]}")
             
             if rank == userRank:
                 username = leaderboard_data[mid][1]
                 
                 start = 1
-                if userRank > 5: # change for 10 max im apge
+                if userRank > 5: 
                     start = userRank - 4
                 limit = start + 9
                 
-                # Ensure limit doesn't exceed total players
+                # Ensure limit doesn't exceed total players; IS THIS NECESSARY?
                 if len(leaderboard_data) < limit:
                     limit = len(leaderboard_data)
                     
