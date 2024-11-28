fb52e6568c7b32d98f4c4389f3ce6bef32552cd6
javidominguezsegura <javier.dominguez.segura@gmail.com>
2024-11-28 22:54:12 +0100

    14/17

--- src/board/grid.py
+++ src/board/grid.py
@@ -6,12 +6,23 @@ from PySide6.QtCore import QSize
 
 class GridLogic:
     def __init__(self, on_cell_click: callable) -> None:
-        """ Defines the logic for the grid """
+        """ Defines the logic for the grid 
+        Time Complexity:
+            - O(1): All operations run in constant time
+        """
         self.grid_size = 5
         self.cells = {}  # Set of all cells in the grid. Each element is a tuple of (row, col)
         self.on_cell_click = on_cell_click  # function to call when a cell is clicked
 
     def setup_grid(self) -> QVBoxLayout:
+        """
+        Description: sets up the grid
+        Time Complexity:
+            - Worst case: O(n^3): where n*n is the size of the grid and the third n is given because
+                 of a very poor hashing function that makes the dictionary insertions collide every time
+            - Average case: O(n^2): where n*n is the size of the grid and when inserting in a set of cells that 
+                are not bombs and the dictionary has a good hashing function
+        """
         self.grid_layout = QGridLayout()
         self.grid_layout.setSpacing(10)  # Spacing between cells
 
@@ -43,7 +54,7 @@ class GridLogic:
                 """)
 
                 self.grid_layout.addWidget(cell, row, col)
-                self.cells[(row, col)] = cell
+                self.cells[(row, col)] = cell # coordinates:button object
 
         grid_container = QVBoxLayout()
         grid_container.addSpacerItem(QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding))
@@ -55,17 +66,29 @@ class GridLogic:
 
         
     def disable_grid(self, disable: bool) -> None:
-        """ Disables all buttons in the grid """
-        for cell in self.cells.values(): # values are the respective buttons
+        """ Description: Disables all buttons in the grid
+        Time Complexity:
+            - O(n): where n is the number of cells in the grid. Note this is pure asymptotic analysis. This ignores the fact that for
+                our game we have fixed the maximum number of cells to 25.
+        """
+        for cell in self.cells.values(): # values are the respective buttons objects 
             cell.setDisabled(disable)
         
     def disable_grid(self, disable: bool) -> None:
+        """ Description: Disables all buttons in the grid
+        Time Complexity:
+            - O(n): where n is the number of cells in the grid. Note this is pure asymptotic analysis. This ignores the fact that for
+                our game we have fixed the maximum number of cells to 25.
+        """
         for cell in self.cells.values(): # values are the respective buttons
             cell.setDisabled(disable)
 
     def reset_buttons(self) -> None:
-        """ Reset the grid to its initial state for a new game"""
-
+        """ Description: Reset the grid to its initial state for a new game
+        Time Complexity:
+            - O(n): where n is the number of cells in the grid. Note this is pure asymptotic analysis. This ignores the fact that for
+                our game we have fixed the maximum number of cells to 25.
+        """
         for cell in self.cells.values():
             cell.setIcon(QIcon())  # Clear the icon
             cell.setEnabled(True)  # Enable to click on the cell
@@ -88,7 +111,11 @@ class GridLogic:
 
 
     def set_button_state(self, row: int, col: int, is_bomb: bool, revealed: bool = False) -> None:
-        """Changes the image and style of a cell, ensuring immediate icon update with transparent background."""
+        """ Description: Changes the image and style of a cell, ensuring immediate icon update with transparent background.
+        Time Complexity:
+            - Average case: O(1): retrieving value in self.cells dictionary is O(1)
+            - Worst case: O(n): retrieving value in self.cells dictionary is O(n) because of a very poor hashing function
+        """
         cell = self.cells[(row, col)]
         
         # Set the icon based on whether it's a bomb or a star
@@ -123,19 +150,27 @@ class GridLogic:
         # Immediate repaint to ensure the icon and style are updated
         cell.repaint()
 
-
-
     def disable_button(self, row:int, col:int) -> None:
+        """ Description: Disables a specific button in the grid
+        Time Complexity:
+            - Average case: O(1): retrieving value in self.cells dictionary is O(1)
+            - Worst case: O(n): retrieving value in self.cells dictionary is O(n) because of a very poor hashing function
+        """
         self.cells[(row, col)].setDisabled(True)
 
 
     def reveal_cells(self, set_of_mines: set, clicked_cells: set) -> None:
-        """ Reveals all cells that are not clicked"""
-        # Showing all other mines
-        non_clicked_cells = set(self.cells.keys()).difference(clicked_cells)
+        """ Description: Reveals all cells that are not clicked
+        Time Complexity:
+            - Average case: O(n): where n is the number of cells in the grid. Note this is pure asymptotic analysis. This ignores the fact that for
+                our game we have fixed the maximum number of cells to 25.
+            - Worst case: O(n^2): where n is the number of cells in the grid. The second n comes from having a very poor hashing when checking
+                if present (row, col) in set_of_mines
+        """
+        non_clicked_cells = set(self.cells.keys()).difference(clicked_cells) # set theory. This takes O(n) time. Returns the key cells are not clicked
         # Revealing unclicked cells
-        for row, col in non_clicked_cells:
-            if (row, col) in set_of_mines:
+        for row, col in non_clicked_cells: # O(n) too
+            if (row, col) in set_of_mines: # O(1) / O(n)
                 self.set_button_state(row, col, True, revealed=True)
             else:
                 self.set_button_state(row, col, False, revealed=True)
